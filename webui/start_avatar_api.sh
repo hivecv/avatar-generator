58706bba03d47cf022d0fa3e58c184877a4abd5b
jakoppini <jakub@hivecv.com>
2025-04-14 09:32:59 +0200

    new masks with transparency bacground, start script for webui and avatar api

---
+++ webui/start_avatar_api.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "[Avatar API] Waiting for WebUI API..."
+
+bash /docker/start_webui.sh &
+
+while ! nc -z localhost 7860 ; do echo "[Avatar API] Waiting..." && sleep 1 ; done
+
+echo "[Avatar API] WebUI API Available! Running Avatar API..."
+
+export POSTHOG_KEY="$1"
+python3 -m fastapi run /avatar_api.py
