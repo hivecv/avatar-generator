e09ff35c4594ddde987223488688ef144681fb6e
Łukasz Piłatowski <lukpila29@gmail.com>
2025-04-25 10:35:51 +0200

    change bash script to start

--- webui/start_webui.sh
+++ webui/start_webui.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 -u webui.py --listen --port 7860 --api --allow-code --xformers --enable-insecure-extension-access
+python3 -u /stable-diffusion-webui/webui.py --listen --port 7860 --api --allow-code --xformers --enable-insecure-extension-access
